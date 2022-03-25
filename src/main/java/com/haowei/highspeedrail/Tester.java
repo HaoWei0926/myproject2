6dfcb8579d3568f038cc597ca4c9052b8e9e2230
HaoWei <haoweiyu0926@gmail.com>
2022-03-25 12:58:41 +0800

    make a ticket booker program

--- src/main/java/com/haowei/highspeedrail/Tester.java
+++ src/main/java/com/haowei/highspeedrail/Tester.java
@@ -6,7 +6,7 @@ public class Tester {
     public static void main(String[] args) {
         Ticket ticket = new Ticket(Station.TAIPEI, Station.TAICHUNG);
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i <; i++) {
+        for (int i = 0; i <0; i++) {
             System.out.println("Your start station?" + "/n" + "1.Taipei" + "/t" + "2.Taichung" + "/t" + "3.Kaohsiung");
             int choice = Integer.parseInt(scanner.next());
             Station startStation = null;
@@ -29,15 +29,15 @@ public class Tester {
                     finalStation = Station.TAIPEI;
                     break;
                 case 2:
-                    finalStation = Station.TAICHUNG
+                    finalStation = Station.TAICHUNG;
                     break;
                 case 3:
                     finalStation = Station.KAOHSIUNG;
                     break;
             }
             System.out.println("Which types?" + "/n" + "1.Normal" + "/t" + "2.Student" + "/t" + "3.Elder&Youngers(65 years old above or 135cm below)" + "/t" + "4.Round-Trip");
-            int
-            for (int k = 0; k <; k++) {
+            //int
+            for (int k = 0; k <3; k++) {
 
             }
             System.out.println("How many tickets?" + "/n" + "1" + "/t" + "2" + "/t" + "3" + "/t" + "4" + "/t" + "5");
