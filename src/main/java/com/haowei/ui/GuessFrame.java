a404117425a8bdc9c5bd4d032465d3d0ba922b9b
HaoWei0926 <haoweiyu0926@gmail.com>
2022-05-27 13:30:55 +0800

    Guess number 1 to 10 GUI

--- src/main/java/com/haowei/ui/GuessFrame.java
+++ src/main/java/com/haowei/ui/GuessFrame.java
@@ -1,13 +1,19 @@
 package com.haowei.ui;
 
+import org.w3c.dom.ls.LSOutput;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GuessFrame extends JFrame {
-    JButton button = new JButton("Ok");
-    JLabel label = new JLabel("Hola");
+    JButton button = new JButton("Guess");
+    JLabel label = new JLabel("Zzzzz...");
+    JTextField number = new JTextField(5);
+    Random random = new Random();
+    int secret = random.nextInt(10)+1;
     public GuessFrame(){
         super();
         setSize(500,400);
@@ -16,13 +22,27 @@ public class GuessFrame extends JFrame {
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                System.out.println("Hello");
-                label.setText("Hello");
+                int num = Integer.parseInt(number.getText());
+                System.out.print(num);
+                System.out.println(" "+secret);
+                for (int i = 0; i < 5; i++) {
+                    if (num > secret){
+                        label.setText("Smaller");
+                    } else if (num < secret) {
+                        label.setText("Bigger");
+                    } else {
+                        label.setText("You win!");
+                        break;
+                    }
+                }
+                //System.out.println("Hello");
+                //label.setText("Hola");
             }
         });
         setLayout(new FlowLayout());
-        add(label);
+        add(number);
         add(button);
+        add(label);
         setVisible(true);
     }
     public static void main(String[] args) {
